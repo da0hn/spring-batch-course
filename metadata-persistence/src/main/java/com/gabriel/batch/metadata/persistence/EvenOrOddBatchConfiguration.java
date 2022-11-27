3b8d89cfa4c03739f7bc77cc2773c632432d1359
Da0hn <gabrielhondacba@gmail.com>
2022-11-26 20:30:11 -0400

    feat: add `printEvenOrOddJob` using `chunk`, `reader`, `processor` and `writer`

---
+++ metadata-persistence/src/main/java/com/gabriel/batch/metadata/persistence/EvenOrOddBatchConfiguration.java
@@ -0,0 +1,71 @@
+package com.gabriel.batch.metadata.persistence;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.function.FunctionItemProcessor;
+import org.springframework.batch.item.support.IteratorItemReader;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+@Configuration
+@EnableBatchProcessing
+public class EvenOrOddBatchConfiguration {
+
+  private final JobBuilderFactory jobBuilderFactory;
+  private final StepBuilderFactory stepBuilderFactory;
+
+  public EvenOrOddBatchConfiguration(
+    final JobBuilderFactory jobBuilderFactory,
+    final StepBuilderFactory stepBuilderFactory
+  ) {
+    this.jobBuilderFactory = jobBuilderFactory;
+    this.stepBuilderFactory = stepBuilderFactory;
+  }
+
+  @Bean
+  public Job printEvenOrOdd() {
+    return this.jobBuilderFactory
+      .get("printEvenOrOddJob")
+      .start(this.printEvenOrOddStep())
+      .incrementer(new RunIdIncrementer())
+      .build();
+  }
+
+  @Bean
+  public Step printEvenOrOddStep() {
+    return this.stepBuilderFactory
+      .get("printEvenOrOddStep")
+      .<Integer, String>chunk(1)
+      .reader(this.countUntilTen())
+      .processor(this.oddOrEvenProcessor())
+      .writer(this.printWriter())
+      .build();
+
+  }
+
+  private IteratorItemReader<Integer> countUntilTen() {
+    final var data = IntStream.rangeClosed(1, 10)
+      .boxed()
+      .collect(Collectors.toList());
+    return new IteratorItemReader<>(data);
+  }
+
+  private FunctionItemProcessor<? super Integer, String> oddOrEvenProcessor() {
+    return new FunctionItemProcessor<>(
+      (item) -> item % 2 == 0 ? String.format("%d is even", item) : String.format("%d is odd", item)
+    );
+  }
+
+  private ItemWriter<? super String> printWriter() {
+    return items -> items.forEach(System.out::println);
+  }
+
+}
