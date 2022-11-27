229e4183d9d0f0650e54752c9d4e16dd67e484df
Da0hn <gabrielhondacba@gmail.com>
2022-11-27 11:49:44 -0400

    chore: create sub-project `flat-file-reader`

---
+++ flat-file-reader/src/main/java/com/gabriel/batch/flatfile/reader/FlatFileReaderApplication.java
@@ -0,0 +1,13 @@
+package com.gabriel.batch.flatfile.reader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlatFileReaderApplication {
+
+  public static void main(final String[] args) {
+    SpringApplication.run(FlatFileReaderApplication.class, args);
+  }
+
+}
