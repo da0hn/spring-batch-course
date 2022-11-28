ecd027e5974b760d982504e445ec6defdd9218e3
Da0hn <gabrielhondacba@gmail.com>
2022-11-28 14:39:51 -0400

    feat: add implementation of delimited file reader job

--- delimited-file-reader/src/main/java/com/gabriel/batch/delimited/files/reader/Client.java
+++ delimited-file-reader/src/main/java/com/gabriel/batch/delimited/files/reader/Client.java
@@ -1,4 +1,46 @@
 package com.gabriel.batch.delimited.files.reader;
 
-public record Client(String name, String surname, String age, String email) {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Objects;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public final class Client {
+
+  private String name;
+  private String surname;
+  private String age;
+  private String email;
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(this.name, this.surname, this.age, this.email);
+  }
+
+  @Override
+  public boolean equals(final Object obj) {
+    if (obj == this) return true;
+    if (obj == null || obj.getClass() != this.getClass()) return false;
+    final var that = (Client) obj;
+    return Objects.equals(this.name, that.name) &&
+           Objects.equals(this.surname, that.surname) &&
+           Objects.equals(this.age, that.age) &&
+           Objects.equals(this.email, that.email);
+  }
+
+  @Override
+  public String toString() {
+    return "Client[" +
+           "name=" + this.name + ", " +
+           "surname=" + this.surname + ", " +
+           "age=" + this.age + ", " +
+           "email=" + this.email + ']';
+  }
+
 }
--- delimited-file-reader/src/main/java/com/gabriel/batch/delimited/files/reader/jobs/steps/readers/DelimitedFileClientReaderConfiguration.java
+++ delimited-file-reader/src/main/java/com/gabriel/batch/delimited/files/reader/jobs/steps/readers/DelimitedFileClientReaderConfiguration.java
@@ -3,6 +3,7 @@ package com.gabriel.batch.delimited.files.reader.jobs.steps.readers;
 import com.gabriel.batch.delimited.files.reader.Client;
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,7 +18,13 @@ public class DelimitedFileClientReaderConfiguration {
   public FlatFileItemReader<Client> delimitedFileClientReader(
     @Value("#{jobParameters['data']}") final Resource dataResource
   ) {
-    return null;
+    return new FlatFileItemReaderBuilder<Client>()
+      .name("delimitedFileClientReader")
+      .resource(dataResource)
+      .delimited()
+      .names("name", "surname", "age", "email")
+      .targetType(Client.class)
+      .build();
   }
 
 }
